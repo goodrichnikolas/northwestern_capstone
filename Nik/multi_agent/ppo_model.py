48c7ae91d714817d9042da2d071753defb8bd967
Your Name <goodrichnik@gmail.com>
2023-11-04 11:30:41 -0400

    PPO updated

--- Nik/multi_agent/ppo_model.py
+++ Nik/multi_agent/ppo_model.py
@@ -17,25 +17,41 @@ from stable_baselines3.ppo import MlpPolicy as PPOMlpPolicy
 from pettingzoo.sisl import waterworld_v4
 
 
+
+#Alter env_fn with a subclass that will allow communication between agents
+    
+
+
 #Arguments
+#Number of pursuers
 n_pursuers = 2
+#Food
 n_evaders = 2
 n_poisons = 2
+#Number of agents that must touch food to get reward together
 n_coop = 0
+#Black lines
 n_sensors = 10
+#length of the black lines
 sensor_range = 0.2
+#archea base radius. Pursuer: radius, food: 2 x radius, poison: 3/4 x radius
 radius = 0.05
+#radius of obstacle object
 obstacle_radius = 0.2
+#Where the obstacle is located
 obstacle_coord = [(0.5, 0.5)]
 pursuer_max_accel = 0.01
 pursuer_speed = 0.05
+#0 means stationary food
 evader_speed = 0.05
 poison_speed = 0.05
 poison_reward = -1
 food_reward = 1
 encounter_reward = 0.01
+#scaling factor for the negative reward used to penalize large actions
 thrust_penalty = -0.01
 local_ratio = 0.5
+#toggles whether pursuing archea (agent) sensors detect speed of other objects and archea
 speed_features = False
 max_cycles = 1000
 current_count = 0
@@ -123,10 +139,6 @@ if __name__ == "__main__":
 
     env_fn = waterworld_v4
     
-    
-    #Alter env_fn with a subclass 
-    
-    
     env_kwargs = {
         "n_pursuers": n_pursuers,
         "n_evaders": n_evaders,
@@ -151,11 +163,9 @@ if __name__ == "__main__":
     }
 
     # Train a model (takes ~3 minutes on GPU)
-    train_butterfly_supersuit(env_fn, steps=196_608, seed=0, **env_kwargs)
+    train_butterfly_supersuit(env_fn, steps=10_000, seed=0, **env_kwargs)
 
     # Evaluate 10 games (average reward should be positive but can vary significantly)
-    eval(env_fn, num_games=10, render_mode=None, **env_kwargs)
+    eval(env_fn, num_games=10, render_mode="human", **env_kwargs)
 
-    
-    
     
